6eafbb2fd65fb8d1d60b2921406e8018a9494e41
MaomaoChicken <saranrat4007@gmail.com>
2026-05-20 19:09:32 +0700

    fei
    add map and code

---
+++ Assets/feifei/code/CameraMover.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraMover : MonoBehaviour
+{
+    public Image fadeImage;
+    public float fadeSpeed = 2f;
+    private bool isMoving = false;
+
+    public void MoveToMap(Transform destination)
+    {
+        if (!isMoving)
+            StartCoroutine(FadeAndMove(destination));
+    }
+
+    IEnumerator FadeAndMove(Transform destination)
+    {
+        isMoving = true;
+
+        // เฟดดำ
+        float t = 0f;
+        while (t < 1f)
+        {
+            t += Time.deltaTime * fadeSpeed;
+            fadeImage.color = new Color(0, 0, 0, t);
+            yield return null;
+        }
+
+        // ย้ายกล้อง
+        transform.position = new Vector3(
+            destination.position.x,
+            transform.position.y,
+            transform.position.z
+        );
+
+        // เฟดกลับ
+        while (t > 0f)
+        {
+            t -= Time.deltaTime * fadeSpeed;
+            fadeImage.color = new Color(0, 0, 0, t);
+            yield return null;
+        }
+
+        isMoving = false;
+    }
+}
+
---
+++ Assets/feifei/code/change map.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Unity.VisualStudio.Editor;
+using System.Data;
+using UnityEngine;
+using static UnityEditor.SceneView;
+
+public class changemap : MonoBehaviour
+{
+    public CameraMover cameraMover;  // ลาก Main Camera มาใส่
+    public Transform m13;
+    public Transform m21;
+    public Transform m22;
+    public Transform m31;
+    public Transform m32;
+    public Transform m41;
+    public Transform destination;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // ย้ายกล้องจาก m13 ไป m21
+            cameraMover.MoveToMap(destination);
+        }
+    }
+}
+
+
+    
